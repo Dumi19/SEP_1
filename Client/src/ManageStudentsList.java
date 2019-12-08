86cec52d7f246059f8ea137a964a61c33e0a8072
Dumi19 <48820975+Dumi19@users.noreply.github.com>
2019-12-08 19:00:50 +0100

    Fourth change

--- Client/src/ManageStudentsList.java
+++ Client/src/ManageStudentsList.java
@@ -1,43 +1,38 @@
 import java.util.ArrayList;
 
-public class ManageStudentsList
-{
-  private ArrayList<Student> students;
+public class ManageStudentsList {
+    private ArrayList<Student> students;
 
-  public ManageStudentsList()
-  {
-    students = new ArrayList<Student>();
-  }
-
-  public Student getAllStudents()   //idk
-  {
-    return students;
-  }
-
-  public ArrayList<Student> getStudentsByCourse(String course) {
-    ArrayList<Student> tempList = new ArrayList<Student>();
-    for (int i = 0 ; i<students.size(); i++){
-      if(students.get(i).getCourseCode().equals(course)){
-        tempList.add(students.get(i));
-      }
+    public ManageStudentsList() {
+        students = new ArrayList<Student>();
     }
-    return tempList;
-  }
 
-  public void addStudent(Student student)
-  {
-    students.add(student);
-  }
+    public ArrayList<Student> getAllStudents()   //idk
+    {
+        return students;
+    }
 
-  public void removeStudents(Student student)
-  {
-    students.remove(student);
-  }
+    public ArrayList<Student> getStudentsByCourse(String course) {
+        ArrayList<Student> tempList = new ArrayList<Student>();
+        for (int i = 0; i < students.size(); i++) {
+            if (students.get(i).getCourseCode().equals(course)) {
+                tempList.add(students.get(i));
+            }
+        }
+        return tempList;
+    }
 
-  public int getNumberOfStudents()
-  {
-    return students.size();
-  }
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    public void removeStudents(Student student) {
+        students.remove(student);
+    }
+
+    public int getNumberOfStudents() {
+        return students.size();
+    }
 
 
 }
