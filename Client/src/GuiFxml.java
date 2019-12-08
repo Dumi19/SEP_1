9ff3050edd77a74e71cf75fa58e260e4c9be177e
Dumi19 <48820975+Dumi19@users.noreply.github.com>
2019-12-08 19:56:38 +0100

    Fourth change

--- Client/src/GuiFxml.java
+++ Client/src/GuiFxml.java
@@ -3,17 +3,21 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import javax.swing.*;
 import java.io.IOException;
+import java.net.URL;
 
 public class GuiFxml extends Application
 {
   public void start(Stage window) throws IOException
   {
-    window.setTitle("A Simple Window 3");
+    window.setTitle("Planner");
     FXMLLoader loader = new FXMLLoader();
     loader.setLocation(getClass().getResource("GuiSEP.fxml"));
     Scene scene = new Scene(loader.load());
     window.setScene(scene);
     window.show();
+
   }
+
 }
