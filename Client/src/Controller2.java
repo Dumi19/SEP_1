8616dadb41a7a21f0e46aed0457bdb9d9fd2d537
Wojciech <womielczarek@gmail.com>
2019-12-09 16:38:00 +0100

    TabGui2

--- Client/src/Controller2.java
+++ Client/src/Controller2.java
@@ -1,6 +1,5 @@
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.*;
 
 public class Controller2
 {
@@ -9,31 +8,47 @@ public class Controller2
   @FXML private ComboBox<String> examinerBox;
   @FXML private ComboBox<String> classroomBox;
   @FXML private Button submit;
-
-
+  @FXML private DatePicker datePicker;
+  @FXML private Alert alert;
+  @FXML private ListView<Student> StudentList;
+  @FXML private ListView<Teacher> TeachersList;
+  @FXML private ListView<Course> CourseList;
+  @FXML private ListView<Room> RoomsList;
   public void initialize() {
 
-    courseBox.getItems().removeAll(courseBox.getItems());
+    courseBox.getItems().removeAll();
     courseBox.getItems().addAll("SDJ1", "SSE", "RWD1", "MSE", "SEP1", "SDJ2", "DBS1", "SWE1", "SEP2");
     courseBox.setPromptText("Choose course");
 
-    studentsBox.getItems().removeAll(studentsBox.getItems());
+    studentsBox.getItems().removeAll();
     studentsBox.getItems().addAll("1X", "1Y", "1Z", "1D", "2X", "2Y", "2Z", "2D", "3X", "3Y", "3Z", "3D");
     studentsBox.setPromptText("Choose students list");
 
-    examinerBox.getItems().removeAll(examinerBox.getItems());
+    examinerBox.getItems().removeAll();
     examinerBox.getItems().addAll("Examiner1", "Examiner2", "Examiner3", "Examiner4", "Examiner5", "Examiner6", "Examiner7", "Examiner8", "Examiner9");
     examinerBox.setPromptText("Choose examiner");
 
-    classroomBox.getItems().removeAll(classroomBox.getItems());
+    classroomBox.getItems().removeAll();
     classroomBox.getItems().addAll("1", "2", "3", "4", "5", "6", "7", "8", "9");
     classroomBox.setPromptText("Choose classroom");
 
-    submit.setOnAction(e -> printSchedule());
+    submit.setOnAction(e -> showAlertWithHeaderText());
   }
 
   private void printSchedule()
   {
-    System.out.println("Course: "+courseBox.getValue()+"\nStudents: "+studentsBox.getValue()+"\nExaminer: "+examinerBox.getValue()+"\nClassroom: "+classroomBox.getValue());
+    alert.setHeaderText("Alert");
+    alert.setContentText("Course: "+courseBox.getValue()+"\nStudents: "+studentsBox.getValue()+"\nExaminer: "+examinerBox.getValue()+"\nClassroom: "+classroomBox.getValue()+"\nDate: "+datePicker.getValue());
+    alert.showAndWait();
   }
+
+  private void showAlertWithHeaderText() {
+    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+    alert.setTitle("Test Connection");
+    alert.setHeaderText("Results:");
+    alert.setContentText("Course: "+courseBox.getValue()+"\nStudents: "+studentsBox.getValue()+"\nExaminer: "+examinerBox.getValue()+"\nClassroom: "+classroomBox.getValue()+"\nDate: "+datePicker.getValue());
+
+    alert.showAndWait();
+  }
+
 }
